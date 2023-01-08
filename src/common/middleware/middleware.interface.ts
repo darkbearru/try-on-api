706688ac6384ece2b8358023db64bdfa256e6c6e
Alexey Abramenko <a.abramenko@chita.ru>
2023-01-08 14:30:22 +0900

    Add. Middleware Interface

--- src/common/middleware/middleware.interface.ts
+++ src/common/middleware/middleware.interface.ts
@@ -1,5 +1,6 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
+import { CustomRequest } from '../../types/custom';
 
 export interface IMiddleware {
-	execute: (req: Request, res: Response, next: NextFunction) => void;
+	execute: (req: CustomRequest, res: Response, next: NextFunction) => void;
 }
